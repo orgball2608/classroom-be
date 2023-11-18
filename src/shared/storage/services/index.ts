bc46157c52c226f067bcef33e36dea5c4192000a
Huỳnh Minh Quang <quanghuynhwtf@gmail.com>
2023-11-18 09:55:08 +0700

    feat: implement update avatar for user

--- src/shared/storage/services/index.ts
+++ src/shared/storage/services/index.ts
@@ -1,19 +1,19 @@
-// index.ts
-
 import * as AWS from 'aws-sdk';
 
+import { ConfigService } from '@nestjs/config';
 import { Provider } from '@nestjs/common';
 import { SERVICES } from '@src/constants/service';
 
-const S3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_REGION,
-});
-
 export const StorageServiceProvider: Provider<AWS.S3> = {
+  useFactory: (configService: ConfigService) => {
+    return new AWS.S3({
+      accessKeyId: configService.get<string>('aws.awsAccessKeyID'),
+      secretAccessKey: configService.get<string>('aws.awsSerectAccessKey'),
+      region: configService.get<string>('aws.awsRegion'),
+    });
+  },
   provide: SERVICES.STORAGE,
-  useValue: S3,
+  inject: [ConfigService],
 };
 
 export interface UploadedMulterFile {
