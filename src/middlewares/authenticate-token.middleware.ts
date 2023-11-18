d83d2e67f220ca2bb1cb6bc074f980bf5bcc521c
Huỳnh Minh Quang <quanghuynhwtf@gmail.com>
2023-11-18 23:59:06 +0700

    feat: fix token expired exception

--- src/middlewares/authenticate-token.middleware.ts
+++ src/middlewares/authenticate-token.middleware.ts
@@ -23,59 +23,55 @@ export class AuthenticateTokenMiddleware implements NestMiddleware {
     private jwtService: JwtService,
   ) {}
   async use(req: Request, res: Response, next: NextFunction) {
-    try {
-      const authHeader = req.headers.authorization;
-      const token = authHeader && authHeader.split(' ')[1];
+    const authHeader = req.headers.authorization;
+    const token = authHeader && authHeader.split(' ')[1];
 
-      if (!token) {
-        throw new ForbiddenException();
-      }
-
-      const isTokenBlacklisted = await this.redis.get(token);
-
-      if (isTokenBlacklisted) {
-        throw new ForbiddenException(TOKEN_MESSAGES.TOKEN_IS_BLACKLIST);
-      } else {
-        const payload: {
-          id: number;
-          tokenId: string;
-          exp: number;
-        } = this.jwtService.verify(token, {
-          secret: this.config.get<string>('auth.accessTokenSecret'),
-          ignoreExpiration: true,
-        });
-
-        if (!payload) {
-          throw new ForbiddenException();
-        }
-
-        const currentTimeInSeconds = Math.floor(Date.now() / 1000);
-
-        if (payload.exp <= currentTimeInSeconds) {
-          throw new UnauthorizedException(TOKEN_MESSAGES.TOKEN_IS_EXPIRED);
-        }
-
-        const timeRemainingInSeconds = payload.exp - currentTimeInSeconds;
-
-        const session = await this.prisma.session.findUnique({
-          where: {
-            userId_tokenId: {
-              userId: payload.id,
-              tokenId: payload.tokenId,
-            },
-          },
-        });
-
-        if (session && session.tokenDeleted) {
-          await this.redis.set(token, 'true', timeRemainingInSeconds);
-          throw new UnauthorizedException(USERS_MESSAGES.USER_LOGGED_OUT);
-        }
-
-        req.user = payload;
-        next();
-      }
-    } catch (error) {
+    if (!token) {
       throw new TokenInvalidException(TOKEN_MESSAGES.TOKEN_IS_INVALID);
+    }
+
+    const isTokenBlacklisted = await this.redis.get(token);
+
+    if (isTokenBlacklisted) {
+      throw new ForbiddenException(TOKEN_MESSAGES.TOKEN_IS_BLACKLIST);
+    } else {
+      const payload: {
+        id: number;
+        tokenId: string;
+        exp: number;
+      } = this.jwtService.verify(token, {
+        secret: this.config.get<string>('auth.accessTokenSecret'),
+        ignoreExpiration: true,
+      });
+
+      if (!payload) {
+        throw new TokenInvalidException(TOKEN_MESSAGES.TOKEN_IS_INVALID);
+      }
+
+      const currentTimeInSeconds = Math.floor(Date.now() / 1000);
+
+      if (payload.exp <= currentTimeInSeconds) {
+        throw new UnauthorizedException(TOKEN_MESSAGES.TOKEN_IS_EXPIRED);
+      }
+
+      const timeRemainingInSeconds = payload.exp - currentTimeInSeconds;
+
+      const session = await this.prisma.session.findUnique({
+        where: {
+          userId_tokenId: {
+            userId: payload.id,
+            tokenId: payload.tokenId,
+          },
+        },
+      });
+
+      if (session && session.tokenDeleted) {
+        await this.redis.set(token, 'true', timeRemainingInSeconds);
+        throw new UnauthorizedException(USERS_MESSAGES.USER_LOGGED_OUT);
+      }
+
+      req.user = payload;
+      next();
     }
   }
 }
