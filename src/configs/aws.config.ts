b585363f26562d3e1cea52502bf3a3274708a2d7
Huỳnh Minh Quang <quanghuynhwtf@gmail.com>
2023-11-18 09:55:08 +0700

    feat: implement update avatar for user

---
+++ src/configs/aws.config.ts
@@ -0,0 +1,33 @@
+import { AwsConfig } from './config.type';
+import { IsString } from 'class-validator';
+import { registerAs } from '@nestjs/config';
+import { validateConfig } from '@src/common/utils';
+
+class EnvironmentVariablesValidator {
+  @IsString()
+  AWS_ACCESS_KEY_ID: string;
+
+  @IsString()
+  AWS_SECRET_ACCESS_KEY: string;
+
+  @IsString()
+  AWS_PUBLIC_BUCKET_KEY: string;
+
+  @IsString()
+  AWS_CLOUDFRONT_URL: string;
+
+  @IsString()
+  AWS_REGION: string;
+}
+
+export default registerAs<AwsConfig>('aws', () => {
+  validateConfig(process.env, EnvironmentVariablesValidator);
+
+  return {
+    awsAccessKeyID: process.env.AWS_ACCESS_KEY_ID,
+    awsSerectAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    awsPublicBucketsKey: process.env.AWS_PUBLIC_BUCKET_KEY,
+    awsCloudfrontURL: process.env.AWS_CLOUDFRONT_URL,
+    awsRegion: process.env.AWS_REGION,
+  };
+});
