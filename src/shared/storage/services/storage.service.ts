a2d6012bb9d819ba4a5961bb608d72ebb5e120ca
Huỳnh Minh Quang <quanghuynhwtf@gmail.com>
2023-11-19 13:26:58 +0700

    fix: fix aws config

--- src/shared/storage/services/storage.service.ts
+++ src/shared/storage/services/storage.service.ts
@@ -13,9 +13,10 @@ export class StorageService {
 
   async uploadFile(params: { key: string; file: Express.Multer.File }) {
     const { key, file } = params;
-    const bucketName = this.configService.get<string>(
+    const bucketName = this.configService.getOrThrow<string>(
       'aws.awsPublicBucketsKey',
     );
+
     const uploadParams = {
       Bucket: bucketName,
       Key: key,
@@ -23,14 +24,14 @@ export class StorageService {
       ContentType: file.mimetype,
     };
     await this.storageClient.putObject(uploadParams).promise();
-    const cloudfrontURL = this.configService.get<string>(
+    const cloudfrontURL = this.configService.getOrThrow<string>(
       'aws.awsCloudfrontURL',
     );
     return `${cloudfrontURL}${key}`;
   }
 
   deleteFile(key: string) {
-    const bucketName = this.configService.get<string>(
+    const bucketName = this.configService.getOrThrow<string>(
       'aws.awsPublicBucketsKey',
     );
     return this.storageClient
