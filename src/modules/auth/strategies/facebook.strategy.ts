cf0f2e22209ce4eb8f01be9d3a2096cd02cd3823
Huỳnh Minh Quang <quanghuynhwtf@gmail.com>
2023-11-28 00:11:41 +0700

    feat: add config to config service

--- src/modules/auth/strategies/facebook.strategy.ts
+++ src/modules/auth/strategies/facebook.strategy.ts
@@ -1,15 +1,16 @@
 import { Profile, Strategy } from 'passport-facebook';
 
+import { ConfigService } from '@nestjs/config';
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 
 @Injectable()
 export class FacebookStrategy extends PassportStrategy(Strategy, 'facebook') {
-  constructor() {
+  constructor(private readonly configService: ConfigService) {
     super({
-      clientID: process.env.APP_ID,
-      clientSecret: process.env.APP_SECRET,
-      callbackURL: 'http://localhost:3000/facebook/redirect',
+      clientID: configService.get<string>('auth.facebookClientID'),
+      clientSecret: configService.get<string>('auth.facebookClientSecret'),
+      callbackURL: configService.get<string>('auth.facebookCallbackURL'),
       scope: 'email',
       profileFields: ['id', , 'emails', 'name'],
     });
