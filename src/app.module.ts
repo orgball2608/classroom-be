b34bcca9ff583cca5c83d2b42aa027bcc72201b6
Huynh Minh Quang <81631701+orgball2608@users.noreply.github.com>
2023-12-16 00:50:30 +0700

    Implement add health check for dockerfile feature (#80)
    * feat (#74): improve database and resolve some todo
    
    * chore (#74): fix something
    
    * refactor (#74): add course teacher table
    
    * fix (#74): migrate aws sdk to v3
    
    * chore (#74): change language invite mail to english
    
    * refactor (#74): migrate from fcm to `socket.io` for notification push
    
    * chore (#74): remove unuse code
    
    * fix (#74): migrate prisma v4 to v5
    
    * improve (#74): add some env
    
    * chore (#74): fix switch env with command
    
    * chore (#74): stopped tracking `.env.dev` file
    
    * chore (#74): change minor code
    
    * chore (#74): add frozen lockfile when install dependence
    
    * ci (#74): test build and push to docker hub ci
    
    * ci (#74): remove old version
    
    * ci (#74): test ci
    
    * ci (#74): remove unnecessary step
    
    * ci (#74): test ci
    
    * ci (#74): fix ci
    
    * ci (#74): rename ci config file
    
    * ci (#74): fix ci
    
    * ci (#74): add image name to env
    
    * ci (#74): final config ci version
    
    * feat (#74): update prisma schema
    
    * feat (#73): add health-check for server
    
    * chore (#73): change issue template
    
    * ci (#73): add call deploy hook
    
    * feat (#73): add metrics from nest js [skip ci]
    
    * ci (#73): add trigger for develop branch

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,9 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 
 import { AuthModule } from './modules/auth/auth.module';
 import { AuthenticateMiddleware } from './middlewares';
@@ -7,8 +12,10 @@ import { CourseModule } from './modules/courses/course.module';
 import { CustomMailerModule } from './shared/mailer/mailer.module';
 import { EventEmitterModule } from '@nestjs/event-emitter';
 import { GatewayModule } from './shared/gateway/gateway.module';
+import { HealthModule } from './modules/health/health.module';
 import { JwtModule } from '@nestjs/jwt';
 import { PrismaModule } from './shared/prisma/prisma.module';
+import { PrometheusModule } from '@willsoto/nestjs-prometheus';
 import { RedisModule } from './shared/redis/redis.module';
 import { SharedModule } from './shared/shared.module';
 import { UserModule } from './modules/users/user.module';
@@ -33,6 +40,9 @@ import redisConfig from './configs/redis.config';
     CustomMailerModule,
     JwtModule.register({}),
     EventEmitterModule.forRoot(),
+    PrometheusModule.register({
+      path: '/metrics',
+    }),
     PrismaModule,
     RedisModule,
     GatewayModule,
@@ -40,11 +50,28 @@ import redisConfig from './configs/redis.config';
     AuthModule,
     UserModule,
     CourseModule,
+    HealthModule,
   ],
   providers: [],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(AuthenticateMiddleware).exclude('auth/(.*)').forRoutes('*');
+    consumer
+      .apply(AuthenticateMiddleware)
+      .exclude(
+        {
+          path: 'auth',
+          method: RequestMethod.ALL,
+        },
+        {
+          path: 'health',
+          method: RequestMethod.GET,
+        },
+        {
+          path: 'metrics',
+          method: RequestMethod.GET,
+        },
+      )
+      .forRoutes('*');
   }
 }
