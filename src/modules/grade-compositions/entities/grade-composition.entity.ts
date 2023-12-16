e01ae493d19d1459ddddb1d6f632554c7e6687c8
Huỳnh Minh Quang <quanghuynhwtf@gmail.com>
2023-12-17 02:15:19 +0700

    feat (#79): add schema swagger decorator for common crud api

---
+++ src/modules/grade-compositions/entities/grade-composition.entity.ts
@@ -0,0 +1,28 @@
+import { AbstractEntity } from '@src/common/entity/abstract.entity';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class GradeCompositionEntity extends AbstractEntity {
+  @ApiProperty({
+    type: Number,
+    example: 1,
+  })
+  courseId: number;
+
+  @ApiProperty({
+    type: String,
+    example: 'Midterm',
+  })
+  name: string;
+
+  @ApiProperty({
+    type: Number,
+    example: 50,
+  })
+  scale: number;
+
+  @ApiProperty({
+    type: Boolean,
+    example: true,
+  })
+  status: boolean;
+}
