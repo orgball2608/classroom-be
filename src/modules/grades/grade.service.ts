8207dec6bb43c9f5e2647d6216a0fbda7136d4f5
Huỳnh Minh Quang <quanghuynhwtf@gmail.com>
2023-12-18 22:12:58 +0700

    fix (#85): stash grade feature

--- src/modules/grades/grade.service.ts
+++ src/modules/grades/grade.service.ts
@@ -1,11 +1,42 @@
 import { CreateGradeDto } from './dto/create-grade.dto';
+import { GRADE_MESSAGES } from '@src/constants';
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '@src/shared/prisma/prisma.service';
 import { UpdateGradeDto } from './dto/update-grade.dto';
 
 @Injectable()
 export class GradeService {
-  create(createGradeDto: CreateGradeDto) {
-    return createGradeDto;
+  constructor(private readonly prisma: PrismaService) {}
+  async create(
+    userId: number,
+    compositionId: number,
+    { studentId, grade }: CreateGradeDto,
+  ) {
+    const gradeData = await this.prisma.grade.create({
+      data: {
+        student: {
+          connect: {
+            id: studentId,
+          },
+        },
+        gradeComposition: {
+          connect: {
+            id: compositionId,
+          },
+        },
+        createdBy: {
+          connect: {
+            id: userId,
+          },
+        },
+        grade,
+      },
+    });
+
+    return {
+      message: GRADE_MESSAGES.CREATE_GRADE_SUCCESSFULLY,
+      data: gradeData,
+    };
   }
 
   findAll() {
