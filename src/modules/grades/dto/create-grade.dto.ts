d90d7902d7123088abfead497521f8f018e8c108
Huỳnh Minh Quang <quanghuynhwtf@gmail.com>
2023-12-18 22:12:58 +0700

    fix (#85): stash grade feature

--- src/modules/grades/dto/create-grade.dto.ts
+++ src/modules/grades/dto/create-grade.dto.ts
@@ -1,1 +1,13 @@
-export class CreateGradeDto {}
+import { NumberField } from '@src/decorators';
+
+export class CreateGradeDto {
+  @NumberField({
+    example: 2012055,
+  })
+  studentId: number;
+
+  @NumberField({
+    example: 10,
+  })
+  grade: number;
+}
