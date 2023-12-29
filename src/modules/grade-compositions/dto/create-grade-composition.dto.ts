97b54e3639336cc6b86beebcfb2a1438ac87b91e
Huỳnh Minh Quang <quanghuynhwtf@gmail.com>
2023-12-30 03:39:51 +0700

    feat (#89): add custom validator for validate  grade composition scale

--- src/modules/grade-compositions/dto/create-grade-composition.dto.ts
+++ src/modules/grade-compositions/dto/create-grade-composition.dto.ts
@@ -1,9 +1,12 @@
 import { NumberField, StringField } from '@src/decorators';
 
+import { IsValidPercentage } from '../validators/Is-valid-percentage.validator';
+
 export class CreateGradeCompositionDto {
   @StringField({ example: 'Grade composite 1' })
   name: string;
 
-  @NumberField({ example: 20, minimum: 0, maximum: 100 })
+  @NumberField({ example: 20, minimum: 10, maximum: 100 })
+  @IsValidPercentage()
   scale: number;
 }
