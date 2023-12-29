ed5f02316aae32de9bf974ab0b8bd1a23f46cb1c
Huỳnh Minh Quang <quanghuynhwtf@gmail.com>
2023-12-30 03:54:46 +0700

    improve (#89): improve grade api

--- src/modules/grades/dto/update-grade.dto.ts
+++ src/modules/grades/dto/update-grade.dto.ts
@@ -1,4 +1,15 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateGradeDto } from './create-grade.dto';
+import { NumberField, StringField } from '@src/decorators';
 
-export class UpdateGradeDto extends PartialType(CreateGradeDto) {}
+export class UpdateGradeDto {
+  @StringField({
+    example: '2012055',
+  })
+  studentId: string;
+
+  @NumberField({
+    example: 10,
+    min: 0,
+    max: 10,
+  })
+  grade: number;
+}
