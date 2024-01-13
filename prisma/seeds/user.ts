831defd4e2eca9519f69e5103c47f9dd1c41d8d6
Huỳnh Minh Quang <quanghuynhwtf@gmail.com>
2024-01-13 09:08:54 +0700

    chore (#128): remove random seed

--- prisma/seeds/user.ts
+++ prisma/seeds/user.ts
@@ -1,56 +1,24 @@
 import { PrismaClient, UserRole, VerifyStatus } from '@prisma/client';
 
-import { faker } from '@faker-js/faker';
 import { generateHash } from '../../src/common/utils';
 
 const prisma = new PrismaClient();
 
 export const createUser = async () => {
   await prisma.user.upsert({
-    where: { email: 'quanghuynh@gmail.com' },
-    update: {},
-    create: {
-      email: 'quanghuynh@gmail.com',
-      password: generateHash('password'),
-      firstName: faker.person.firstName(),
-      lastName: faker.person.lastName(),
-      phoneNumber: faker.phone.number(),
-      address: faker.location.streetAddress(),
-      avatar: faker.image.avatar(),
-      verify: VerifyStatus.VERIFY,
-      role: UserRole.TEACHER,
-    },
-  });
-
-  await prisma.user.upsert({
     where: { email: 'admin@gmail.com' },
     update: {},
     create: {
       email: 'admin@gmail.com',
       password: generateHash('password'),
-      firstName: faker.person.firstName(),
-      lastName: faker.person.lastName(),
-      phoneNumber: faker.phone.number(),
-      address: faker.location.streetAddress(),
-      avatar: faker.image.avatar(),
+      firstName: 'Nguyễn Văn',
+      lastName: 'Anh',
+      phoneNumber: '0914894337',
+      address: 'Thành Phố Thủ Đức, Hồ Chí Minh',
+      avatar:
+        'https://d25ezbt2biq5zr.cloudfront.net/2c5a8d03-c6f9-4933-b99e-1eb26067083e',
       verify: VerifyStatus.VERIFY,
-      role: 'ADMIN',
-    },
-  });
-
-  await prisma.user.upsert({
-    where: { email: 'student@gmail.com' },
-    update: {},
-    create: {
-      email: 'student@gmail.com',
-      password: generateHash('password'),
-      firstName: faker.person.firstName(),
-      lastName: faker.person.lastName(),
-      phoneNumber: faker.phone.number(),
-      address: faker.location.streetAddress(),
-      verify: VerifyStatus.VERIFY,
-      avatar: faker.image.avatar(),
-      role: 'STUDENT',
+      role: UserRole.ADMIN,
     },
   });
 };
