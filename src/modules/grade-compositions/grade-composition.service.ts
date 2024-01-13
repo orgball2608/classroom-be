ec1c2dbbfa5384e71d39236df55b2af2152232ed
Huỳnh Minh Quang <quanghuynhwtf@gmail.com>
2024-01-13 12:48:01 +0700

    fix (#128): remove all grade when delete grade composition

--- src/modules/grade-compositions/grade-composition.service.ts
+++ src/modules/grade-compositions/grade-composition.service.ts
@@ -137,6 +137,12 @@ export class GradeCompositionService {
 
   async remove(id: number): Promise<ApiResponseOmitDataEntity> {
     await this.prisma.$transaction(async (tx) => {
+      await tx.grade.deleteMany({
+        where: {
+          gradeCompositionId: id,
+        },
+      });
+
       const deletedGradeComposition = await tx.gradeComposition.delete({
         where: {
           id,
